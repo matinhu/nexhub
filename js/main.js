23038895c3dc8241ab5133725f433900eb94cede
matinhu dev <matheulls@hotmail.com>
2023-10-18 21:27:31 -0300

    Feature: Reestruturação de pastas, js iniciado e login iniciado

--- js/main.js
+++ js/main.js
@@ -2,25 +2,50 @@ document.addEventListener('DOMContentLoaded', () => {
   // Toggle between Login and Register
   const toggleToRegister = document.getElementById('toggleToRegister');
   const toggleToLogin = document.getElementById('toggleToLogin');
-  const loginForm = document.getElementById('loginForm');
-  const registerForm = document.getElementById('registerForm');
+
+  const toggleForms = (show, hide) => {
+    show.classList.replace('d-none', 'd-block');
+    hide.classList.replace('d-block', 'd-none');
+  };
 
   toggleToRegister.addEventListener('click', () => {
-    console.log('botão registrar clicado')
-    loginForm.style.display = 'none';
-    registerForm.style.display = 'block';
+    toggleForms(
+      document.getElementById('registerForm'),
+      document.getElementById('loginForm')
+    );
   });
 
   toggleToLogin.addEventListener('click', () => {
-    registerForm.style.display = 'none';
-    loginForm.style.display = 'block';
+    toggleForms(
+      document.getElementById('loginForm'),
+      document.getElementById('registerForm')
+    );
   });
 
   // Mock API Calls
   document.getElementById('login').addEventListener('submit', function (e) {
     e.preventDefault();
-    // Replace this with your actual API call
-    console.log('API call to login endpoint');
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+
+    fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const token = data.token
+        const user = data.user
+        document.getElementById("loggedPerson").textContent = `Bem vindo(a), ${user.username}`;
+        console.log('Login Response:', data);
+        // Handle the login response here
+      })
+      .catch((error) => console.error('Login Error:', error));
+
   });
 
   document.getElementById('register').addEventListener('submit', function (e) {
